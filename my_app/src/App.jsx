be66d8da2571ade4533bd36b4da5b515a40517c5
Rensytimsy <rensytimsy03@gmail.com>
2023-11-13 18:43:42 +0300

    Created the hero section

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -1,10 +1,12 @@
 // import {React} from "react"
 import Nav_bar from "../src/my_components/nav_bar"
+import Hero from "../src/my_components/hero"
 import "../src/index.css"
 function App() {
   return (
     <>
     <Nav_bar />
+    <Hero />
     </>
   )
 }
---
+++ my_app/src/my_components/hero.jsx
@@ -0,0 +1,10 @@
+
+export default function Hero(){
+    return(
+        <div className="hero-div">
+            <img className="hero-image" src="Group 77.png" />
+            <h1 className="hero-h">Online Experiences</h1>
+            <p className="hero-p">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
