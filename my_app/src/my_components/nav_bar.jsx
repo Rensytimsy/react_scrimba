aaae8185ed944b967db59af050c0c42d562cec07
Rensytimsy <rensytimsy03@gmail.com>
2023-11-13 18:12:39 +0300

    airbnb nav bar set up

---
+++ my_app/src/my_components/nav_bar.jsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+export default function Nav_bar(){
+    return(
+        <div className="nav-bar">
+            <div className="logo">
+                <img className="logo-image" src="/airbnb 1.png"/>
+            </div>
+        </div>
+    )
+}
